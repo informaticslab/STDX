0bb8abf17055ce58b2b85442c4e21e18001b6318
Greg Ledbetter <gsledbetter@gmail.com>
2016-09-15 14:37:51 -0400

    Initial checkin.

---
+++ STDX/GuidelinesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace STDX
+{
+	public partial class GuidelinesPage : ContentPage
+	{
+		public GuidelinesPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ STDX/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace STDX
+{
+	public partial class MainPage : TabbedPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
